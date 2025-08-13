8416772888ed79f106e12a3750d6897d33b2009e
Archie To <toanhminh0412@gmail.com>
2025-08-12 23:44:21 -0700

    Create a header

---
+++ woocommerce-rental-addon.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Rental Addon
+ * Plugin URI: https://example.com/
+ * Description: Adds rental functionality to WooCommerce products.
+ * Version: 0.1.0
+ * Author: Archie
+ * Author URI: https://example.com/
+ * Text Domain: woocommerce-rental-addon
+ * Domain Path: /languages
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+\texit; // Exit if accessed directly.
+}
+
+
